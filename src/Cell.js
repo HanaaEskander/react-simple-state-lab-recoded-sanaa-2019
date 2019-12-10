5c52d489348df5972cfd17d9dbf3f1f23d1c3186
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-10 06:55:54 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,36 +1,24 @@
 import React, { Component } from 'react';
 
-export default class Cell extends Component {
-     
-    constructor(props) {
-        super()
+export default class Cell extends Component{
+    constructor(props){
+        super();
         this.state = {
-            color:props.value,
-<<<<<<< HEAD
-        } 
-        // ...define initial state with a key of 'color' set to the 'value' prop
+          color:props.value,
+        }
       }
-     newColor=(color)=>{
-      this.setState({
-          color:color
-=======
-        } // ...define initial state with a key of 'color' set to the 'value' prop
-      }
-      updatState=()=>{
-      this.setState({
-          color:'#333'
->>>>>>> 9b01038afdd53a630a4a332911e86f77d5095d14
-      })}
 
-  render() {
-    return (
-<<<<<<< HEAD
-      <div className="cell" style={{backgroundColor:this.state.color}} onClick={()=>this.newColor( '#333')}>
-=======
-      <div className="cell" style={{backgroundColor:this.state.color}} onClick={this.updatState}>
->>>>>>> 9b01038afdd53a630a4a332911e86f77d5095d14
-      </div>
-    )
-  }
-  
+      changeColor = (color)=>{
+          this.setState({
+              color:color
+          });
+      }
+
+    render(){
+        return(
+            <div className="cell" style={{backgroundColor: this.state.color}} onClick={()=>this.changeColor( '#333')}>
+                {this.props.value}
+            </div>
+        );
+    }
 }
