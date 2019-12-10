24c941e709cac791a17699d200ce967b4362fe63
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-10 06:55:14 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -6,17 +6,29 @@ export default class Cell extends Component {
         super()
         this.state = {
             color:props.value,
+<<<<<<< HEAD
         } 
         // ...define initial state with a key of 'color' set to the 'value' prop
       }
      newColor=(color)=>{
       this.setState({
           color:color
+=======
+        } // ...define initial state with a key of 'color' set to the 'value' prop
+      }
+      updatState=()=>{
+      this.setState({
+          color:'#333'
+>>>>>>> 9b01038afdd53a630a4a332911e86f77d5095d14
       })}
 
   render() {
     return (
+<<<<<<< HEAD
       <div className="cell" style={{backgroundColor:this.state.color}} onClick={()=>this.newColor( '#333')}>
+=======
+      <div className="cell" style={{backgroundColor:this.state.color}} onClick={this.updatState}>
+>>>>>>> 9b01038afdd53a630a4a332911e86f77d5095d14
       </div>
     )
   }
--- src/Matrix.js
+++ src/Matrix.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import Cell from './Cell';
 
 export default class Matrix extends Component {
+<<<<<<< HEAD
   
   genRow = (vals) => (
   vals.map(val => <Cell value={val} />) 
+=======
+  genRow = (vals) => (
+    vals.map(val => <Cell value={val} />) 
+>>>>>>> 9b01038afdd53a630a4a332911e86f77d5095d14
     // replace me and render a cell component instead!
   )
   
@@ -22,7 +27,11 @@ export default class Matrix extends Component {
   
 }
 
+<<<<<<< HEAD
 Matrix.defaultProps={
+=======
+Matrix.defaultProps = {
+>>>>>>> 9b01038afdd53a630a4a332911e86f77d5095d14
   values:[
     ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
     ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
